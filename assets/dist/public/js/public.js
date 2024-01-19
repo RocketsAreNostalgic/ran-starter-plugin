6469bc57b74da157e02e3985ff0eedf6cd47efc7
bnjmnrsh <bnjmnrsh@gmail.com>
2024-01-19 18:34:51 +0000

    build

---
+++ assets/dist/public/js/public.js
@@ -0,0 +1,4 @@
+/*! my-plugin v0.0.1 | (c) 2024 Benjamin Rush | MIT License | https://github.com/RocketsAreNostalgic */
+
+document.addEventListener("DOMContentLoaded",(function(){console.log("public scripts here")}));
+//# sourceMappingURL=public.js.map
