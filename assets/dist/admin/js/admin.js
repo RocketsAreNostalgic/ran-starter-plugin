3b166b7a0e8e1e4ef85853775f114594e5c07d70
bnjmnrsh <bnjmnrsh@gmail.com>
2024-01-21 01:16:59 +0000

    build

--- assets/dist/admin/js/admin.js
+++ assets/dist/admin/js/admin.js
@@ -1,4 +1,3 @@
-/*! my-plugin v0.0.1 | (c) 2024 Benjamin Rush | MIT License | https://github.com/RocketsAreNostalgic */
-
-document.addEventListener("DOMContentLoaded",(function(){console.log("Admin scripts are here!!!")}));
+/*! ran-starter-plugin v0.0.1 | (c) 2024 Benjamin Rush | MIT License | https://github.com/RocketsAreNostalgic */
+document.addEventListener("DOMContentLoaded",function(){console.log("Admin scripts are here!!!")});
 //# sourceMappingURL=admin.js.map
--- assets/dist/public/js/public.js
+++ assets/dist/public/js/public.js
@@ -1,4 +1,3 @@
-/*! my-plugin v0.0.1 | (c) 2024 Benjamin Rush | MIT License | https://github.com/RocketsAreNostalgic */
-
-document.addEventListener("DOMContentLoaded",(function(){console.log("public scripts here")}));
+/*! ran-starter-plugin v0.0.1 | (c) 2024 Benjamin Rush | MIT License | https://github.com/RocketsAreNostalgic */
+document.addEventListener("DOMContentLoaded",function(){console.log("public scripts here")});
 //# sourceMappingURL=public.js.map
